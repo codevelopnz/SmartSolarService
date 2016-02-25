464a645f6089adabfc1a9fb0e74d3f6ba4cba855
Andrew Cave <andrewcave@live.com>
2016-02-25 16:25:28 +1300

    Added more device settings, reference to Azure ElasticScale

--- Codevelop.Service/Codevelop.Shared.Entity/DeviceSettings.cs
+++ Codevelop.Service/Codevelop.Shared.Entity/DeviceSettings.cs
@@ -1,15 +1,32 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Codevelop.Shared.Entity
 {
     public class DeviceSettings
     {
+        public DeviceSettings()
+        {
+            TargetTempAt = new List<DateTimeOffset>();
+        }
+
         public Guid Id { get; set; }
 
         public Guid DeviceId { get; set; }
 
+        public bool Active { get; set; }
+
+        public string SettingsDescription { get; set; }
+
         public int MinimumTemp { get; set; }
 
         public int TargetTemp { get; set; }
+
+
+        public List<DateTimeOffset> TargetTempAt { get; set; }
+
+        public DateTimeOffset? BoostUntil { get; set; }
+
+
     }
 }
